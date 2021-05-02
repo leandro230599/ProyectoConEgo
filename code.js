d63f8597d3eb87284588b4ff4808491017b957e1
Tiotata <goffredoezequiel@gmail.com>
2021-05-02 19:24:44 -0300

    ahora se cachea la cantidad de mesas  y no empieza en el html la mesa 0  lo probe y andaba deje un comentario para ver si podemos mejorar una cosita

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
-x=0;
+//ESTO SE REPITE CODIGO PERO COMO NOSE BIEN MANEJAR ESTO HABRIA Q VER  TECNICAMENTE ES IGUAAL A AGREGAR MESA 
 const btnAdd = document.getElementById('agregarmesa');
 btnAdd.addEventListener('click', function(){ 
 const article = document.createElement('article');
 article.classList.add('article');
 const h1 = document.createElement('h1');
-h1.innerHTML="Mesa"+" "+x;
+var Cantidad = localStorage.getItem("Cantidad");
+h1.innerHTML="Mesa"+" "+Cantidad;
 h1.classList.add('titulomesa');
  
 
@@ -32,10 +33,66 @@ article.appendChild(img);
 article.appendChild(p);
 article.appendChild(div);
 elemento = document.getElementById('section');
+Cantidad++;
+localStorage.setItem("Cantidad", Cantidad); 
 
 elemento.appendChild(article);
-x++;}, false);
+
+}, false);
 function hola () { let hola="hola"
 asdasdasdas;
 }
+
+window.onload=function() {
+    var Cantidad = localStorage.getItem("Cantidad");
+    if (Cantidad != null){
+        for (let index = 0; index < Cantidad; index++) {
+            agregarmesa(index);
+        }
+
+    }
+   
+}
+
+
+function agregarmesa (ID) {
+    const article = document.createElement('article');
+    article.classList.add('article');
+    const h1 = document.createElement('h1');
+    h1.innerHTML="Mesa"+" "+ID;
+    h1.classList.add('titulomesa');
+     
+    
+    const img = document.createElement('img');
+    img.classList.add('libre');
+    img.src='images/libre.png';
+    
+    const p = document.createElement('p');
+    const div= document.createElement('div');
+    div.classList.add('probando');
+    const a1 = document.createElement('a');
+    const a2 = document.createElement('a');
+    const a3 = document.createElement('a');
+    a1.innerHTML="Abrir Mesa ";
+    a2.innerHTML="Cerrar Mesa ";
+    a3.innerHTML="Agregar Producto ";
+    a1.classList.add('AbrirMesa');
+    a2.classList.add('CerrarMesa');
+    a3.classList.add('AgregarProduct');
+    div.appendChild(a1);
+    div.appendChild(a2);
+    div.appendChild(a3);
+    article.appendChild(h1);
+    article.appendChild(img);
+    article.appendChild(p);
+    article.appendChild(div);
+    elemento = document.getElementById('section');
+    article.setAttribute("id", ID);
+    elemento.appendChild(article);
+    ID++;
+    localStorage.setItem("Cantidad", ID); 
+
+    
+}
+
 
